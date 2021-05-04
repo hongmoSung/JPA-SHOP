5caaedc19a1526ae52143cc0f523049465ee8a2c
hongmo sung <tesung30@naver.com>
2021-05-04 18:29:50 +0900

    프로젝트 생성

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setHello("hello");
+		final String hello1 = hello.getHello();
+		System.out.println(hello1);
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
