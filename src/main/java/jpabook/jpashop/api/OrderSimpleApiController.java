26310f6351880be4252d63aadabbd3653be586dc
hongmo sung <tesung30@naver.com>
2021-05-11 19:46:51 +0900

    간단한 주문 조회 V3:엔티티를 DTO로 변환 - 패치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -45,6 +45,13 @@ public class OrderSimpleApiController {
         return result;
     }
 
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        final List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream().map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
     @Data
     static class SimpleOrderDto {
         private Long orderId;
